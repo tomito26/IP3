bc221f17a2489273f455d7dcfae99ea8066559a0
tomito26 <tommybwah@gmail.com>
2020-07-25 18:16:36 +0300

    added jquery effect on what we do section wheren by once you click the image it dissappears and  a description appears and vice versa

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-$(document).ready('')
+$(document).ready(function(){
+    $('.clickable').click(function() {
+        $('#design').toggle();
+        $('#icon').toggle();
+        
+    });
+    $('.dev').click(function() {
+        $('#dev_icon').toggle()
+        $('#development').toggle()
+        
+    });
+    $('.management').click(function() {
+        $('#prod-icon').toggle()
+        $('#product').toggle()
+        
+    });
+
+});
